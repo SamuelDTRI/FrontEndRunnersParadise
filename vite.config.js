0352d66a3b4d7e6095c4ac5ffc32bfb307f0b0fb
sariditri31@gmail.com <sariditri31@gmail.com>
2024-01-29 16:18:16 -0500

    plumitoas

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import react from "@vitejs/plugin-react-swc";
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default {
@@ -6,4 +6,12 @@ export default {
   server: {
     port: 5173,
   },
+  build: {
+    optimizeDeps: {
+      exclude: ['@mui/icons-material/Close'],
+    },
+  },
 };
+
+
+
